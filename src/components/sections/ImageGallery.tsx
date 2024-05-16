e46e938fc889ac01951b3b53a0b41599b3294aef
oh-ji-yeon <o.delayed0106@gmail.com>
2024-05-16 17:23:57 +0900

    이미지 최적화

--- src/components/sections/ImageGallery.tsx
+++ src/components/sections/ImageGallery.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Section from '@shared/Section'
 import styles from './ImageGallery.module.scss'
 import ImageViewer from '@/components/ImageViewer'
+import generateImageUrl from '@/utils/generateImageUrl'
 
 const cx = classNames.bind(styles)
 
@@ -30,7 +31,24 @@ function ImageGallery({ images }: { images: string[] }) {
                 handleSelectedImage(idx)
               }}
             >
-              <img src={src} alt="사진첩 이미지" />
+              <picture>
+                <source
+                  srcSet={generateImageUrl({
+                    filename: src,
+                    format: 'webp',
+                    option: 'w_240,h_240,q_auto,c_fill',
+                  })}
+                  type="image/webp"
+                />
+                <img
+                  src={generateImageUrl({
+                    filename: src,
+                    format: 'jpg',
+                    option: 'w_240,h_240,q_auto,c_fill',
+                  })}
+                  alt="이미지"
+                />
+              </picture>
             </li>
           ))}
         </ul>
